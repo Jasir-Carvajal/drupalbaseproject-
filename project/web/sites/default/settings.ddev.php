158163acd8ff09c2c907c1dc7231487ea77d2eaa
Jasir Carvajal <jasircaso67@gmail.com>
2023-03-27 23:30:13 +0000

    Parches y assets packagist

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'UnHckqHCNzANfisuyYaqsPwUOTsSDABLIuCIDNYUFxonwaYDpgEwmRCzvBdSxQfK';
+$settings['hash_salt'] = 'gcErjqXsGEWBcsHUETxAGgDPSnFgFHOyFOlBuEPjMivpWWpHzJBCuyTKpApQaStG';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;

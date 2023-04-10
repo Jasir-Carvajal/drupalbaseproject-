11e6ffbbedaa2f2169369f56087efb9c7ce75526
Jasir Carvajal <jasircaso67@gmail.com>
2023-04-10 02:29:54 +0000

    Chef configuration

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'VvmSwdxkmUvywQTbYvfpqUITmazsNYfQpoUNSefNbcdroahHZQQtfbNoLbKOLZNZ';
+$settings['hash_salt'] = 'zCPhoIDlrERkiMfYQFwgnCHhJGxCPkFEjlogFiCnIRSxbACGMdRVVOLDmemddPNL';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;

316c29ceed66d55969b9a6a901f629185587f8c0
Jasir Carvajal <jasircaso67@gmail.com>
2023-05-02 00:42:27 +0000

    Entity media browser

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'YOhAJmaXYUTBdqouxZIdevSTglCwXlODXRvQGVDZdmGkDYRDFtSCgUPqLWtNhmVk';
+$settings['hash_salt'] = 'WpAEweqeluSKbSwIRDUfwqtxYtNOaFBnwDfNIWPRtWXfDTvEsjuEfHhdQCepXbzr';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
